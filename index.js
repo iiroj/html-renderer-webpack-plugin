e49fddc19e3a7ea0c39bf4b73ae54a16402ab41c
Iiro Jäppinen <iiro@jappinen.fi>
2018-10-02 20:54:22 +0300

    fix: Remove rogue console.log

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ const defaultRenderer = async ({ assets, publicPath }) =>
 const invalidateRequireCache = hotPathRegex => {
   for (const id of Object.keys(require.cache)) {
     if (!id.includes('node_modules') && hotPathRegex.test(id)) {
-      console.log(id);
       delete require.cache[id];
     }
   }
