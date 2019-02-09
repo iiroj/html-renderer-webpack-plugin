4f8514874903b0a71405af961cd8a908ef8a7c8d
Iiro Jäppinen <iiro@jappinen.fi>
2019-02-09 15:33:40 +0200

    feat: build using Rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from "rollup-plugin-typescript2";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/HtmlRendererWebpackPlugin.ts",
+  output: {
+    file: pkg.main,
+    format: "cjs"
+  },
+  external: [
+    ...Object.keys(pkg.dependencies),
+    "fs",
+    "path"
+  ],
+  plugins: [
+    typescript({
+      typescript: require("typescript"),
+      tsconfig: './tsconfig.build.json'
+    })
+  ]
+};
