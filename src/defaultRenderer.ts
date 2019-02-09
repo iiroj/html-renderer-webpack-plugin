3297a6111fa15d5c1baa8a9bc3f597ea8c83f7ab
Iiro Jäppinen <iiro@jappinen.fi>
2019-02-09 15:11:56 +0200

    refactor: defaultRenderer uses template string

--- src/defaultRenderer.ts
+++ src/defaultRenderer.ts
@@ -3,13 +3,11 @@ import getScriptTags from "./getScriptTags";
 export default async ({
   assets,
   publicPath
-}: import("./HtmlRendererWebpackPlugin").RendererArgs) =>
-  "<!doctype html>" +
-  "<head>" +
-  '<meta charset="utf-8">' +
-  "<title>HtmlRendererWebpackPlugin</title>" +
-  '<meta name="viewport" content="width=device-width, initial-scale=1">' +
-  "</head>" +
-  '<div id="root">' +
-  "</div>" +
-  getScriptTags(publicPath, assets ? assets.js : []);
+}: import("./HtmlRendererWebpackPlugin").RendererArgs) => `<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <title>HtmlRendererWebpackPlugin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<div id="root"></div>
+${getScriptTags(publicPath, assets ? assets.js : [])}`;
